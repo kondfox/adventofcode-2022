23d7aeb7942722af6240b32e878a0efb8c1a90bd
Kond <peter.tamas@greenfoxacademy.com>
2022-12-08 00:07:35 +0100

    feat(dec_07): ugly but short JS solution

---
+++ dec_07/dec07-ugly-short.js
@@ -0,0 +1,32 @@
+import { readFileLines } from '../utils/fileIO.js'
+
+const dirStack = []
+const dirSizes = {}
+
+const operations = {
+  cdIn: dirName => dirStack.unshift(`/${dirName}${dirStack[0] || ''}`),
+  cdOut: () => dirSizes[dirStack[1]] += dirSizes[dirStack.shift()],
+  addSize: size => dirSizes[dirStack[0]] = dirSizes[dirStack[0]] + size || size,
+}
+
+const parse = ([p1, p2, p3]) => {
+  if (p3 === '..') operations.cdOut()
+  else if (p2 === 'cd') operations.cdIn(p3)
+  else if (!!Number.parseInt(p1)) operations.addSize(Number.parseInt(p1))
+}
+
+const calculateDirSizes = filePath => {
+  readFileLines(filePath).map(line => line.split(' ')).forEach(parse)
+  while (dirStack.length > 1) operations.cdOut()
+}
+
+const q1 = m => Object.values(dirSizes).filter(s => s <= m).reduce((sum, s) => sum + s, 0)
+
+const q2 = (total, need) => Object.entries(dirSizes)
+  .sort((ds1, ds2) => ds1[1] - ds2[1])
+  .find(([_, size]) => size >= need - (total - dirSizes['//']))[1]
+
+calculateDirSizes('dec_07/sample-input.txt')
+
+console.log(q1(100_000))
+console.log(q2(70_000_000, 30_000_000))
--- utils/fileIO.js
+++ utils/fileIO.js
@@ -5,3 +5,7 @@ const rootDir = new URL('..', import.meta.url).pathname
 export const readFile = (filePath) => {
   return fs.readFileSync(rootDir + filePath, 'utf-8')
 }
+
+export const readFileLines = (filePath) => {
+  return readFile(filePath).split('\n')
+}
