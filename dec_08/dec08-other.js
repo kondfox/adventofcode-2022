f4d54ddbf8c8e644fd3c5c76a4e53c7867d64a83
Kond <peter.tamas@greenfoxacademy.com>
2022-12-09 14:26:09 +0100

    feat(dec_08): 2. part better JS solution

--- dec_08/dec08-other.js
+++ dec_08/dec08-other.js
@@ -7,6 +7,8 @@ const direction = (width, all) => ({
   W: i => (i % width != 0 ? i - 1 : undefined),
 })
 
+const indexArray = arr => arr.map((_, i) => i)
+
 function findVisibles(filePath) {
   const forest = readFileLines(filePath).map(line =>
     line.split('').map(tree => Number(tree))
@@ -21,23 +23,53 @@ function findVisibles(filePath) {
   return new Set([...edges, ...localMaxes]).size
 }
 
+function findHighestScore(filePath) {
+  const forest = readFileLines(filePath).map(line =>
+    line.split('').map(tree => Number(tree))
+  )
+  const width = forest[0].length
+  const trees = forest.flatMap(t => t)
+  const dir = direction(width, trees.length)
+  return indexArray(trees).reduce(
+    (highest, t) =>
+      Math.max(
+        highest,
+        Object.values(dir).reduce((score, d) => {
+          const s = nextLocalMax(trees, t, d)?.steps || 0
+          return score * s
+        }, 1)
+      ),
+    0
+  )
+}
+
 function findEdges(width, trees) {
-  return trees
-    .map((_, i) => i)
-    .filter(
-      i =>
-        i < width ||
-        i >= trees.length - width ||
-        i % width == 0 ||
-        (i + 1) % width == 0
-    )
+  return indexArray(trees).filter(
+    i =>
+      i < width ||
+      i >= trees.length - width ||
+      i % width == 0 ||
+      (i + 1) % width == 0
+  )
+}
+
+function nextLocalMax(trees, tree, dir, allowEquals = true, max = tree) {
+  let steps = 0
+  while (dir(tree)) {
+    steps++
+    tree = dir(tree)
+    if ((allowEquals && trees[tree] >= trees[max]) || trees[tree] > trees[max])
+      return { steps, index: tree }
+  }
+  return { steps, index: undefined }
 }
 
 function findLocalMaxes(trees, tree, dir) {
   const maxes = [tree]
-  while (dir(tree)) {
-    tree = dir(tree)
-    if (trees[tree] > trees[maxes[0]]) maxes.unshift(tree)
+  let next = nextLocalMax(trees, tree, dir, false, maxes[0])
+  while (next.index != undefined) {
+    maxes.unshift(next.index)
+    next = nextLocalMax(trees, tree, dir, false, maxes[0])
   }
   maxes.pop()
   return maxes
@@ -45,3 +77,6 @@ function findLocalMaxes(trees, tree, dir) {
 
 console.log(findVisibles('dec_08/sample-input.txt'))
 console.log(findVisibles('dec_08/input.txt'))
+
+console.log(findHighestScore('dec_08/sample-input.txt'))
+console.log(findHighestScore('dec_08/input.txt'))
