423a039f714a4d8032d48548b8b74431632e2c99
Kond <peter.tamas@greenfoxacademy.com>
2022-12-10 21:05:41 +0100

    refactor(dec_10): refactor JS part 1 solution

--- dec_10/dec10.js
+++ dec_10/dec10.js
@@ -1,32 +1,32 @@
 import { readFileLines } from '../utils/fileIO.js'
 
+const common = cpu => {
+  cpu.i % 40 == 20 && (cpu.strength += cpu.i * cpu.x)
+  cpu.tasks[cpu.i] && cpu.tasks[cpu.i]()
+}
+
 const ops = {
-  noop: cpu => (cpu.i += 1),
-  addx: (cpu, v, tasks) => {
-    tasks[cpu.i + 1] = () => (cpu.x += Number(v))
-    cpu.i += 2
+  noop: cpu => {
+    common(cpu)
+    cpu.i += 1
+  },
+  addx: (cpu, v) => {
+    common(cpu)
+    cpu.tasks[cpu.i + 1] = () => (cpu.x += Number(v))
+    cpu.i += 1
+    common(cpu)
+    cpu.i += 1
   },
 }
 
 function signalStrengthSum(filePath) {
   const lines = readFileLines(filePath)
-  const cpu = { x: 1, i: 1 }
-  const tasks = {}
+  const cpu = { x: 1, i: 1, strength: 0, tasks: {} }
   for (let row of lines) {
     const [op, v] = row.split(' ')
-    ops[op](cpu, v, tasks)
+    ops[op](cpu, v)
   }
-  let signalStrength = 0
-  for (let i = 1; Object.keys(tasks).length; i++) {
-    if (i % 40 == 20) {
-      signalStrength += i * cpu.x
-    }
-    if (tasks[i]) {
-      tasks[i]()
-      delete tasks[i]
-    }
-  }
-  return signalStrength
+  return cpu.strength
 }
 
 console.log(signalStrengthSum('dec_10/sample-input1.txt'))
