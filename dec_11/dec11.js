fda1d0f711e624a7d8f3a834849786220a09c7c4
Kond <peter.tamas@greenfoxacademy.com>
2022-12-12 00:24:43 +0100

    feat(dec_11): part 1 JS solution

---
+++ dec_11/dec11.js
@@ -0,0 +1,62 @@
+import { readFileLines } from '../utils/fileIO.js'
+
+const monkey = () => items => op => cond => case1 => case2 => ({
+  items,
+  op,
+  test: n => (n % cond == 0 ? case1 : case2),
+  inspects: 0,
+})
+
+const operation = {
+  '*': v => n => n * (v || n),
+  '+': v => n => n + (v || n),
+}
+
+const parseOperation = ([_, o, v]) => operation[o](Number(v))
+
+function parseMonkeys(filePath) {
+  const lines = readFileLines(filePath)
+  const states = {
+    skip: () => {},
+    items: row =>
+      row
+        .split(': ')[1]
+        .split(', ')
+        .map(i => Number(i)),
+    op: row => parseOperation(row.split(' = ')[1].split(' ')),
+    testCond: row => Number(row.trim().split(' ')[3]),
+    testCase1: row => Number(row.trim().split(' ')[5]),
+    testCase2: row => Number(row.trim().split(' ')[5]),
+  }
+  const stateOrder = [...Object.values(states), states.skip]
+  const monkeys = []
+  for (let j = 0; j < (lines.length + 1) / stateOrder.length; j++) {
+    let m = monkey()
+    for (let i = 0; i < stateOrder.length; i++) {
+      const result = stateOrder[i](lines[j * stateOrder.length + i])
+      result != undefined && (m = m(result))
+    }
+    monkeys.push(m)
+  }
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < monkeys.length; j++) {
+      const m = monkeys[j]
+      const itemCount = m.items.length
+      m.inspects += itemCount
+      for (let k = 0; k < itemCount; k++) {
+        const item = m.items.shift()
+        const worry = Math.floor(m.op(item) / 3)
+        const to = m.test(worry)
+        monkeys[to].items.push(worry)
+      }
+    }
+  }
+  return monkeys
+    .map(m => m.inspects)
+    .sort((a, b) => a - b)
+    .slice(-2)
+    .reduce((p, i) => p * i, 1)
+}
+
+console.log(parseMonkeys('dec_11/sample-input.txt'))
+console.log(parseMonkeys('dec_11/input.txt'))
