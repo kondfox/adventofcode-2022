af9c7ac167bdf342ecfa1b3aba8be8b56987c3c7
Kond <peter.tamas@greenfoxacademy.com>
2022-12-14 12:35:29 +0100

    feat(dec_13): 2. part JS solution

--- dec_13/dec13.js
+++ dec_13/dec13.js
@@ -5,34 +5,62 @@ const asArray = n => (Array.isArray(n) ? n : [n])
 function sumOfInOrderPairIndices(filePath) {
   const input = parseInput(filePath)
   let inOrder = 0
-  for (let i = 0, j = 1; i < input.length; i += 3, j++) {
+  for (let i = 0, j = 1; i < input.length; i += 2, j++) {
     const [arr1, arr2] = [input[i], input[i + 1]]
     if (compareArrays(arr1, arr2)) inOrder += j
   }
   return inOrder
 }
 
-function parseInput(filePath) {
-  return readFileLines(filePath).map(row => {
-    const stack = []
-    for (let i = 0; i < row.length; i++) {
-      let c = row[i]
-      if (c === ',') continue
-      if (c === '[') stack.unshift([])
-      else if (c === ']') {
-        if (stack.length > 1) {
-          const embedded = stack.shift()
-          stack[0].push(embedded)
-        }
-      } else {
-        if (row[i + 1] && row[i + 1] !== ',' && row[i + 1] !== ']') {
-          c += row[++i]
-        }
-        stack[0].push(Number(c))
-      }
+function findDecoderKey(filePath) {
+  const sorted = sort(filePath)
+  let [key2, key6] = [0, 0]
+  for (let i = 0; i < sorted.length; i++) {
+    if (key2 > 0 && key6 > 0) break
+    if (sorted[i].length == 1 && sorted[i][0].length == 1) {
+      if (sorted[i][0][0] === 2) key2 = i + 1
+      else if (sorted[i][0][0] === 6) key6 = i + 1
     }
-    return stack[0]
-  })
+  }
+  return key2 * key6
+}
+
+function sort(filePath) {
+  const input = parseInput(filePath)
+  input.push([[2]], [[6]])
+  input.sort(arrayComparator)
+  return input
+}
+
+function parseInput(filePath) {
+  return readFileLines(filePath)
+    .map(row => {
+      const stack = []
+      for (let i = 0; i < row.length; i++) {
+        let c = row[i]
+        if (c === ',') continue
+        if (c === '[') stack.unshift([])
+        else if (c === ']') {
+          if (stack.length > 1) {
+            const embedded = stack.shift()
+            stack[0].push(embedded)
+          }
+        } else {
+          if (row[i + 1] && row[i + 1] !== ',' && row[i + 1] !== ']') {
+            c += row[++i]
+          }
+          stack[0].push(Number(c))
+        }
+      }
+      return stack[0]
+    })
+    .filter(arr => arr !== undefined)
+}
+
+function arrayComparator(arr1, arr2) {
+  const comp = compareArrays(arr1, arr2)
+  if (comp === undefined) return 0
+  return comp ? -1 : 1
 }
 
 function compareArrays(arr1, arr2) {
@@ -52,3 +80,6 @@ function compareArrays(arr1, arr2) {
 
 console.log(sumOfInOrderPairIndices('dec_13/sample-input.txt'))
 console.log(sumOfInOrderPairIndices('dec_13/input.txt'))
+
+console.log(findDecoderKey('dec_13/sample-input.txt'))
+console.log(findDecoderKey('dec_13/input.txt'))
