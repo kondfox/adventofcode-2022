dfeb6fb273255379100aaabed3cfbca05ec38fea
Kond <peter.tamas@greenfoxacademy.com>
2022-12-16 15:11:39 +0100

    feat(dec_15): 2. part JS solution

--- dec_15/dec15.js
+++ dec_15/dec15.js
@@ -1,35 +1,55 @@
 import { readFileLines } from '../utils/fileIO.js'
-import { CoordinateSet } from '../utils/datastructures.js'
+import { Intervals } from '../utils/datastructures.js'
 
-function notBeaconPositionsInLine(filePath, line) {
-  const detections = readFileLines(filePath)
+function part1(filePath, line) {
+  return notBeaconPositionsInLine(parseDevices(filePath), line).longest()
+}
+
+function part2(filePath, limit) {
+  const [x, y] = findBeaconPosition(parseDevices(filePath), limit)
+  return x * 4_000_000 + y
+}
+
+function findBeaconPosition(devices, limit) {
+  for (let i = 0; i <= limit; i++) {
+    const intervals = notBeaconPositionsInLine(devices, i)
+    if (intervals.size() < 2) continue
+    const [[x1, x2], [x3, x4]] = intervals.inOrder()
+    return [x2 + 1, i]
+  }
+}
+
+function notBeaconPositionsInLine(devices, line) {
+  return devices
+    .map(({ sensor, beacon }) => pointsInLine(sensor, beacon, line))
+    .reduce((merged, intervals) => {
+      merged.merge(intervals)
+      return merged
+    })
+}
+
+function parseDevices(filePath) {
+  return readFileLines(filePath)
     .map(row => row.match(/[-]*[0-9]+/g))
     .map(([x1, y1, x2, y2]) => ({
       sensor: [Number(x1), Number(y1)],
       beacon: [Number(x2), Number(y2)],
     }))
-    .map(({ sensor, beacon }) => {
-      const points = pointsInLine(sensor, beacon, line)
-      return points
-    })
-    .reduce((points, set) => {
-      points.union(set)
-      return points
-    })
-  return detections.size()
 }
 
 function pointsInLine([sx, sy], [bx, by], l) {
   let dist = Math.abs(sx - bx) + Math.abs(sy - by)
   if ([sy - dist, sy + dist, l].sort((m, n) => m - n)[1] !== l) {
-    return new CoordinateSet()
+    return new Intervals()
   }
-  const points = new CoordinateSet()
-  for (let x = -(dist - Math.abs(l - sy)); x <= dist - Math.abs(l - sy); x++) {
-    if (!(sx + x === bx && l === by)) points.add([sx + x, l])
-  }
-  return points
+  return new Intervals([
+    sx - (dist - Math.abs(l - sy)),
+    sx + dist - Math.abs(l - sy),
+  ])
 }
 
-console.log(notBeaconPositionsInLine('dec_15/sample-input.txt', 10))
-console.log(notBeaconPositionsInLine('dec_15/input.txt', 2_000_000))
+// console.log(part1('dec_15/sample-input.txt', 10))
+// console.log(part1('dec_15/input.txt', 2_000_000))
+
+// console.log(part2('dec_15/sample-input.txt', 20))
+console.log(part2('dec_15/input.txt', 4_000_000))
--- utils/datastructures.js
+++ utils/datastructures.js
@@ -1,3 +1,39 @@
+export function Intervals(initData) {
+  const intervals = []
+
+  const overlapping = (i1, i2) =>
+    (i1[0] <= i2[0] && i1[1] >= i2[0]) || (i2[0] <= i1[0] && i2[1] >= i1[0])
+
+  const add = int => {
+    if (!intervals.length || !intervals.some(i => overlapping(i, int))) {
+      intervals.push(int)
+    } else {
+      const newInterval = [...int]
+      for (let i = 0; i < intervals.length; i++) {
+        if (overlapping(intervals[i], newInterval)) {
+          newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+          newInterval[1] = Math.max(newInterval[1], intervals[i][1])
+          intervals.splice(i--, 1)
+        }
+      }
+      intervals.push(newInterval)
+    }
+  }
+
+  const merge = otherIntervals => otherIntervals.intervals.forEach(i => add(i))
+
+  const inOrder = () => intervals.sort((i1, i2) => i1[0] - i2[0])
+
+  const size = () => intervals.length
+
+  const longest = () =>
+    intervals.reduce((l, i) => (i[1] - i[0] > l ? i[1] - i[0] : l), 0)
+
+  initData && add(initData)
+
+  return { intervals, add, merge, inOrder, size, longest }
+}
+
 export function CoordinateSet(initData) {
   const idSet = new Set()
   const w = 1_000_000
@@ -13,6 +49,7 @@ export function CoordinateSet(initData) {
     [...idSet]
       .map(id => id.match(/[-]*[0-9]+/g))
       .map(([x, y]) => [Number(x), Number(y)])
+      .sort((a, b) => a[0] - b[0])
 
   const size = () => idSet.size
 
